150602634fc1d94466000ebe93ca00be64610263
Crinax <agreedeth@gmail.com>
2021-11-22 21:42:26 +0300

    Deploy... Fixed

--- server.ts
+++ server.ts
@@ -11,6 +11,14 @@ const app = express();
 
 app.use(cors());
 
+app.all('*', function(req, res, next) {
+     var origin = req.get('origin'); 
+     res.header('Access-Control-Allow-Origin', origin);
+     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+     res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+});
+
 app.use('/api/:version/:method', async (req, res) => {
   const method = req.params.method as MethodT;
   const api = APIFactory.get(req.params.version, [
